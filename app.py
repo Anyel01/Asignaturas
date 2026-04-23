d971eb724ba55d91934ff919bd30292274dd7445
Anyelo Steven Oliveros Rivera <anyelo201931@gmail.com>
2026-04-23 13:44:54 -0500

    Refactor: Cambiar estructura de boletín a distribución de carga académica por docentes

--- app.py
+++ app.py
@@ -20,44 +20,19 @@ def generar():
 @app.route('/imprimir', methods=['POST'])
 def imprimir():
     data = request.get_json()
-    # Forzar la institución a "Colegio Gonzalo Rivera Laguado"
-    data['institucion'] = 'Colegio Gonzalo Rivera Laguado'
 
     asignaturas = data.get('asignaturas', [])
-    periodos = data.get('periodos', [])
-    num_periodos = len(periodos)
-
-    for asig in asignaturas:
-        horas_vals = []
-        for h in asig.get('horas', []):
-            try:
-                if h != '' and h is not None:
-                    horas_vals.append(float(h))
-            except (ValueError, TypeError):
-                pass
-        if horas_vals:
-            asig['promedio'] = round(sum(horas_vals) / len(horas_vals), 1)
-            asig['total'] = round(sum(horas_vals), 1)
-        else:
-            asig['promedio'] = 0
-            asig['total'] = 0
-
-    todos_promedios = [a['promedio'] for a in asignaturas if a['promedio'] > 0]
-    promedio_general = round(sum(todos_promedios) / len(todos_promedios), 1) if todos_promedios else 0
-    total_horas_global = sum(a['total'] for a in asignaturas)
+    
+    # Calcular total_horas como suma de intensidades
+    total_horas = sum(asig.get('intensidad', 0) for asig in asignaturas)
 
     html = render_template('boletin_impresion.html',
-        institucion=data.get('institucion', ''),
-        programa=data.get('programa', ''),
-        anio=data.get('anio', datetime.now().year),
-        responsable=data.get('responsable', ''),
-        observaciones=data.get('observaciones', ''),
+        docente=data.get('docente', ''),
+        titular=data.get('titular', ''),
+        tipo_docente=data.get('tipo_docente', ''),
         asignaturas=asignaturas,
-        periodos=periodos,
-        num_periodos=num_periodos,
-        promedio_general=promedio_general,
-        total_horas_global=total_horas_global,
-        fecha=datetime.now().strftime('%d/%m/%Y')
+        total_horas=total_horas,
+        mes_anio=datetime.now().strftime('%B de %Y')
     )
     response = make_response(html)
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
